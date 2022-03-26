726dcf24a450698ad361dd6e766577bb12ebdd38
OliB3456 <peter.jan464@gmail.com>
2022-03-26 14:01:09 +0100

    First commit

---
+++ Lab00/src/main/java/com/example/lab00/Lab00Application.java
@@ -0,0 +1,14 @@
+package com.example.lab00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab00Application {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(Lab00Application.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ Lab00/src/test/java/com/example/lab00/Lab00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
