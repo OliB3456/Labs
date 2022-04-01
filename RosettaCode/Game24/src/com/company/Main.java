74752f66f2881cd7cc90278522a93c0fdce8890b
OliB3456 <q5rU7BwuILZThFFE1WUv>
2022-04-01 22:55:38 +0200

    RosettaCode/Game24: Read user-input; Add repeat

--- RosettaCode/Game24/src/com/company/Main.java
+++ RosettaCode/Game24/src/com/company/Main.java
@@ -1,18 +1,26 @@
 package com.company;
 
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Generating 4 digits...");
-        int[] digits = IntStream.generate(ThreadLocalRandom.current()::nextInt)
-                .filter(i -> 0 <= i && i <= 9)
-                .limit(4)
-                //.peek(i -> System.out.println(""+i))
-                .toArray();
-        System.out.print("Put in a mathematical expression (+-/*) that evaluates to 24 with these 4 digits: " + intArrToString(digits));
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Generating 4 digits...");
+            int[] digits = IntStream.generate(ThreadLocalRandom.current()::nextInt)
+                    .filter(i -> 0 <= i && i <= 9)
+                    .limit(4)
+                    //.peek(i -> System.out.println(""+i))
+                    .toArray();
+            System.out.println("Put in a mathematical expression (+-/*) that evaluates to 24 with these 4 digits: " + intArrToString(digits));
+            System.out.print("Expression: ");
+            String input = scanner.nextLine();
+
+            System.out.println("Do you want to play another round?");
+        } while(repeat(scanner.nextLine()));
     }
 
     private static String intArrToString(int[] arr) {
@@ -23,5 +31,12 @@ public class Main {
         }
         s = s.substring(0, s.length()-2);
         return s;
+    }
+
+    private static boolean repeat(String repeat) {
+        if(!repeat.isBlank()) {
+           return repeat.equalsIgnoreCase("yes") || repeat.equalsIgnoreCase("y");
+        }
+        return false;
     }
 }
